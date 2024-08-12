5ce8f58136236bc00efca2cf4fe6455e97a2e358
NDorves <dorves@gmx.net>
2024-08-12 12:15:40 +0200

    Update HAFG_37.py

--- HAFG_37.py
+++ HAFG_37.py
@@ -33,7 +33,7 @@ import mysql.connector
 dbconfig = {
     'host': 'ich-db.ccegls0svc9m.eu-central-1.rds.amazonaws.com',
     'user': 'ich1',
-    'password': 'password',
+    'password': 'XXXXXX',
     'database': 'ich_edit'
 }
 connection = mysql.connector.connect(**dbconfig)
